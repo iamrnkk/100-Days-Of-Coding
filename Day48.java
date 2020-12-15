112f7db2cfa73f1b89cfd853c1afcdfce6239f4f
Kamalakar (Raunak) Srivastava <iamrnkk@gmail.com>
2020-12-16 01:24:52 +0530

    Add Day 48's Solution (Debugging Phase)

--- Day48.java
+++ Day48.java
@@ -26,9 +26,48 @@ public class Day48
 	static HashMap<Character,Integer> charDirs= new HashMap<>();
 	static {
 		charDirs.put('N', N);
-		charDirs.put('N', E);
+		charDirs.put('E', E);
 		charDirs.put('S', S);
 		charDirs.put('W', W);
+	}
+	
+	public static int getOppositeDir(int dir)
+	{
+		return (dir + 2) % 4;
+	}
+	
+	public static void addEdges(Vertex fromVertex, Vertex toVertex, int dir)
+	{
+		int oppDir = getOppositeDir(dir);
+		
+		fromVertex.edges.get(dir).add(toVertex);
+		toVertex.edges.get(oppDir).add(fromVertex);
+		
+		for(int refDir: DIRECTIONS)
+		{
+			if(refDir == dir)
+				continue;
+			
+			for(Vertex neighbourDirVertex: fromVertex.edges.get(refDir))
+			{
+				if(neighbourDirVertex == toVertex)
+					continue;
+				
+				neighbourDirVertex.edges.get(dir).add(toVertex);
+				toVertex.edges.get(oppDir).add(neighbourDirVertex);
+			}
+		}
+	}
+	
+	public static boolean isValidRule(Vertex fromVertex, Vertex toVertex, int dir)
+	{
+		int oppDir= getOppositeDir(dir);
+		System.out.println(dir);
+		System.out.println(fromVertex.edges.get(oppDir));
+		if(fromVertex.edges.get(oppDir).contains(toVertex))
+			return false;
+		
+		return true;
 	}
 	
 	public static boolean validateRules(String[] rules)
@@ -59,37 +98,26 @@ public class Day48
 			
 			for(char charDir: pointsAndDirs[1].toCharArray())
 			{
+				System.out.println(charDir+" "+charDirs);
 				int dir = charDirs.get(charDir);
-			}
-			
-		}
-	}	
-		public static void addEdges(HashMap<Character,Vertex> pointsMap, Vertex toVertex, Vertex fromVertex, int dir)
-		{
-			int oppDir = getOppositeDir(dir);
-			
-			fromVertex.edges.get(dir).add(toVertex);
-			toVertex.edges.get(oppDir).add(fromVertex);
-			
-			for(int refDir: DIRECTIONS)
-			{
-				if(refDir == dir)
-					continue;
 				
-				for(Vertex neighbourDirVertex: fromVertex.edges.get(refDir))
-				{
-					if(neighbourDirVertex == toVertex)
-						continue;
-					
-					neighbourDirVertex.edges.get(dir).add(toVertex);
-					toVertex.edges.get(oppDir).add(neighbourDirVertex);
-				}
+				if(!isValidRule(fromVertex, toVertex, dir))
+					return false;
+				
+				addEdges(fromVertex, toVertex, dir);
 			}
 		}
 		
-		public static int getOppositeDir(int dir)
-		{
-			return (dir + 2) % 4;
+		return true;
 		}
 	
-}
+	public static void main(String args[])
+	{
+		String[] rules = {
+							"A N B",
+							"B NE C",
+							"C N A"
+						  };
+		System.out.println(validateRules(rules));
+		}
+	}
